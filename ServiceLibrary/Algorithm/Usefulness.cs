cb57326463257b8fcf6322370983b1f97b2c9ce7
PatrickSlag <patrickslag@gmail.com>
2024-04-18 10:35:29 +0200

    NT-145 Usefulness Counter

--- ServiceLibrary/Algorithm/Usefulness.cs
+++ ServiceLibrary/Algorithm/Usefulness.cs
@@ -17,5 +17,16 @@ namespace ServiceLibrary.Algorithm
             if (target.Inventory.GetInventoryAmount(trade.OfferedItem) + trade.OfferedAmount == 5) return _random.NextDouble() > 0.5f;
             return false;
         }
+        
+        public Trade CalculateCounter(Trade trade, Tribe target)
+        {
+            foreach (InventoryItems resource in Enum.GetValues(typeof(InventoryItems)))
+            {
+                var newTrade = new Trade(trade.RequestedItem, trade.RequestedAmount, resource, trade.OfferedAmount);
+                if (Calculate(newTrade, target)) return newTrade;
+            }
+
+            return trade;
+        }
     }
 }
