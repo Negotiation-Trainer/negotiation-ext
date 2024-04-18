f10911021478c83a127a0c86fa00c5ab177b09ce
PatrickSlag <patrickslag@gmail.com>
2024-04-18 10:35:00 +0200

    NT-145 Selfbuild Counter

--- ServiceLibrary/Algorithm/SelfBuild.cs
+++ ServiceLibrary/Algorithm/SelfBuild.cs
@@ -17,5 +17,16 @@ namespace ServiceLibrary.Algorithm
             if (target.Inventory.GetInventoryAmount(trade.RequestedItem) == SelfBuildThreshold) return (_random.NextDouble() > 0.5f);
             else return target.Inventory.GetInventoryAmount(trade.RequestedItem) < SelfBuildThreshold;
         }
+
+        public Trade CalculateCounter(Trade trade, Tribe target)
+        {
+            foreach (InventoryItems resource in Enum.GetValues(typeof(InventoryItems)))
+            {
+                var newTrade = new Trade(resource, trade.RequestedAmount, trade.OfferedItem, trade.OfferedAmount);
+                if (Calculate(newTrade, target)) return newTrade;
+            }
+
+            return trade;
+        }
     }
 }
