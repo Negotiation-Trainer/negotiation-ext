170ec7b281bc3b5460bf55bae28592989f4832a9
Jarno Crans <84608017+VarioCode@users.noreply.github.com>
2024-04-25 14:56:17 +0200

    NT-310, Fixed Algo tests

--- ServiceLibraryTest/AlgorithmServiceTests.cs
+++ ServiceLibraryTest/AlgorithmServiceTests.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using ModelLibrary;
 using ModelLibrary.Exceptions;
@@ -20,6 +19,7 @@ namespace ServiceLibraryTest
             _random = new Random();
             _tradeOriginator = new Tribe("A");
             _tradeTarget = new Tribe("B");
+            
         }
         // A Test behaves as an ordinary method
         [Test]
@@ -49,7 +49,7 @@ namespace ServiceLibraryTest
         }
         
         [Test]
-        public void CalculateSelfBuild_HigerThanBorder_ReturnsFalse()
+        public void CalculateSelfBuild_HigherThanBorder_ReturnsFalse()
         {
             //Given
             SelfBuild selfBuild = new SelfBuild(_random);
@@ -216,14 +216,94 @@ namespace ServiceLibraryTest
         {
             // Given
             var algorithmService = new AlgorithmService() {EnableRandomizedDecisions = false};
+            FillPointsTable();
+            FillInventory();
 
             // When & Then
             algorithmService.AlgorithmDecision += (sender, args) =>
             {
-                Assert.That(args.issuesWithTrade.Count, Is.EqualTo(expectedException.Count));
+                    Assert.That(args.issuesWithTrade.Count, Is.EqualTo(expectedException.Count));
                 args.issuesWithTrade.ForEach(issue => Assert.That(expectedException, Does.Contain(issue.GetType())));
             };
             algorithmService.Decide(trade, _tradeOriginator, _tradeTarget);
+        }
+        
+        private void FillInventory()
+        {
+            _tradeOriginator.Inventory.AddToInventory(InventoryItems.Wood, 6);
+            _tradeOriginator.Inventory.AddToInventory(InventoryItems.Lenses, 6);
+            _tradeOriginator.Inventory.AddToInventory(InventoryItems.Clay, 1);
+            _tradeOriginator.Inventory.AddToInventory(InventoryItems.Gold, 6);
+            _tradeOriginator.Inventory.AddToInventory(InventoryItems.Steel, 2);
+            _tradeOriginator.Inventory.AddToInventory(InventoryItems.Insulation, 0);
+            _tradeOriginator.Inventory.AddToInventory(InventoryItems.Fertilizer, 2);
+            _tradeOriginator.Inventory.AddToInventory(InventoryItems.Stone, 2);
+        
+            _tradeTarget.Inventory.AddToInventory(InventoryItems.Wood, 4);
+            _tradeTarget.Inventory.AddToInventory(InventoryItems.Lenses, 2);
+            _tradeTarget.Inventory.AddToInventory(InventoryItems.Clay, 4);
+            _tradeTarget.Inventory.AddToInventory(InventoryItems.Gold, 6);
+            _tradeTarget.Inventory.AddToInventory(InventoryItems.Steel, 5);
+            _tradeTarget.Inventory.AddToInventory(InventoryItems.Insulation, 3);
+            _tradeTarget.Inventory.AddToInventory(InventoryItems.Fertilizer, 3);
+            _tradeTarget.Inventory.AddToInventory(InventoryItems.Stone, 6);
+        }
+
+        private void FillPointsTable()
+        {
+            _tradeOriginator.PointTable = new Dictionary<(InventoryItems, Tribe), int>
+            {
+                [(InventoryItems.Wood, _tradeOriginator)] = 10,
+                [(InventoryItems.Wood, _tradeTarget)] = -5,
+
+                [(InventoryItems.Lenses, _tradeOriginator)] = 10,
+                [(InventoryItems.Lenses, _tradeTarget)] = 5,
+
+                [(InventoryItems.Clay, _tradeOriginator)] = 10,
+                [(InventoryItems.Clay, _tradeTarget)] = 0,
+
+                [(InventoryItems.Gold, _tradeOriginator)] = 10,
+                [(InventoryItems.Gold, _tradeTarget)] = 5,
+
+                [(InventoryItems.Steel, _tradeOriginator)] = 10,
+                [(InventoryItems.Steel, _tradeTarget)] = 0,
+
+                [(InventoryItems.Insulation, _tradeOriginator)] = 10,
+                [(InventoryItems.Insulation, _tradeTarget)] = 5,
+                
+                [(InventoryItems.Fertilizer, _tradeOriginator)] = 10,
+                [(InventoryItems.Fertilizer, _tradeTarget)] = 0,
+
+                [(InventoryItems.Stone, _tradeOriginator)] = 10,
+                [(InventoryItems.Stone, _tradeTarget)] = 0,
+            };
+            
+            _tradeTarget.PointTable = new Dictionary<(InventoryItems, Tribe), int>
+            {
+                [(InventoryItems.Wood, _tradeOriginator)] = 0,
+                [(InventoryItems.Wood, _tradeTarget)] = 10,
+
+                [(InventoryItems.Lenses, _tradeOriginator)] = -5,
+                [(InventoryItems.Lenses, _tradeTarget)] = 10,
+
+                [(InventoryItems.Clay, _tradeOriginator)] = 5,
+                [(InventoryItems.Clay, _tradeTarget)] = 10,
+
+                [(InventoryItems.Gold, _tradeOriginator)] = 5,
+                [(InventoryItems.Gold, _tradeTarget)] = 10,
+
+                [(InventoryItems.Steel, _tradeOriginator)] = 0,
+                [(InventoryItems.Steel, _tradeTarget)] = 10,
+
+                [(InventoryItems.Insulation, _tradeOriginator)] = 5,
+                [(InventoryItems.Insulation, _tradeTarget)] = 10,
+
+                [(InventoryItems.Fertilizer, _tradeOriginator)] = 0,
+                [(InventoryItems.Fertilizer, _tradeTarget)] = 10,
+
+                [(InventoryItems.Stone, _tradeOriginator)] = 0,
+                [(InventoryItems.Stone, _tradeTarget)] = 10,
+            };
         }
         public static TestCaseData[] TestCasesForDecideMethod
         {
@@ -232,9 +312,24 @@ namespace ServiceLibraryTest
                 var array = new[]
                 {
                     new TestCaseData(
-                        new Trade(InventoryItems.Wood, 2, InventoryItems.Stone, 1, "A", "B"),
+                        new Trade(InventoryItems.Clay,1,InventoryItems.Lenses,1, "A", "B"),
+                        new List<Type> { typeof(UsefulnessException) }
+                        ),
+                    new TestCaseData(
+                        new Trade(InventoryItems.Gold, 3, InventoryItems.Wood, 3, "A", "B"),
+                        new List<Type> { typeof(SelfBuildException) }
+                    ),
+                    new TestCaseData(
+                        new Trade(InventoryItems.Clay, 2, InventoryItems.Stone, 1, "A", "B"),
                         new List<Type> { typeof(TradeBalanceException) }
-                    )
+                    ),
+                    new TestCaseData(
+                        new Trade(InventoryItems.Lenses, 1, InventoryItems.Stone, 1, "A", "B"),
+                        new List<Type> { typeof(BuildEffectException) }
+                    ),
+                    new TestCaseData(
+                        new Trade(InventoryItems.Insulation,1,InventoryItems.Wood,2, "A", "B"),
+                        new List<Type>()) // No exceptions;
                     // Add more test cases as needed
                 };
                 return array;
