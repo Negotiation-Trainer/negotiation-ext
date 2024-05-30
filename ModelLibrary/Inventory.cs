d0780e47aa1a1e4d60079a432112cc9328a29148
Jarno Crans <84608017+VarioCode@users.noreply.github.com>
2024-05-30 14:57:59 +0200

    NT-386, added more code comments

--- ModelLibrary/Inventory.cs
+++ ModelLibrary/Inventory.cs
@@ -17,17 +17,32 @@ namespace ModelLibrary
             }
         }
         
+        /// <summary>
+        /// Get the amount of an item in the inventory
+        /// </summary>
+        /// <param name="item">The Inventory Item</param>
+        /// <returns></returns>
         public int GetInventoryAmount(InventoryItems item)
         {
             return _inventory[item];
         }
 
+        /// <summary>
+        /// x
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="amount"></param>
         public void AddToInventory(InventoryItems item, int amount)
         {
             _inventory[item] += amount;
             InventoryUpdate?.Invoke(this, EventArgs.Empty);
         }
 
+        /// <summary>
+        /// Remove a set amount of a item in the inventory.
+        /// </summary>
+        /// <param name="item">The inventory item</param>
+        /// <param name="amount">The amount to remove</param>
         public void RemoveFromInventory(InventoryItems item, int amount)
         {
             if(_inventory[item] == 0) return;
@@ -35,6 +50,9 @@ namespace ModelLibrary
             InventoryUpdate?.Invoke(this, EventArgs.Empty);
         }
 
+        /// <summary>
+        /// invoke the inventory update event
+        /// </summary>
         public void UpdateInventory()
         {
             InventoryUpdate?.Invoke(this,EventArgs.Empty);
